23f081eb405e04330651394a2d01d9da70405abc
aragheb <abdallah.ragheb@squadio.com>
2024-10-17 23:55:34 +0300

    test

--- scripts/start_node_app.sh
+++ scripts/start_node_app.sh
@@ -4,7 +4,7 @@
 sudo chmod -R 777 /home/ec2-user/cicd
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /home/ec2-user/cicd
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
