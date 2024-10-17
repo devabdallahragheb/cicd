93d4d7fefcaf380cec036ec4ff9c6239f4a32bce
aragheb <abdallah.ragheb@squadio.com>
2024-10-17 23:26:03 +0300

    test

--- scripts/start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/cicd
-# Replace with your start command
-sudo npm install -g pm2s
--- scripts/stop_node_app.sh
+++ scripts/stop_node_app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Replace with the command to stop your Node.js app
+echo "Stopping any existing node servers"
 pkill -f node
