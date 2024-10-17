872bda290f69780e6c9b1275fc4969b5845cff18
aragheb <abdallah.ragheb@squadio.com>
2024-10-18 00:16:28 +0300

    fix

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,6 +6,14 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-rm -rf /home/ec2-user/cicd/*
+ 
+#create our working directory if it doesnt exist
+DIR="/home/ec2-user/cicd"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
+fi
 
  
