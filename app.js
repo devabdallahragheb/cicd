09f77752672466e81db3f3ac4ea326495bca3a56
Abdullah abdelglil <82816002+devabdallahragheb@users.noreply.github.com>
2024-10-17 10:28:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,19 +14,7 @@ app.get('/test', (req, res) => {
     res.send('Hello health!');
   });
   
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-  //test
-=======
-  //
-=======
-  /////
->>>>>>> 6987ab3d6a4cb8116ed18b6137f457c7bc622ed0
->>>>>>> 32e879098d90df83e6b201a8bdbea12fe89d04f7
-// Start the server
->>>>>>> 8c65b44942c6e2d01980abdaa45cbcf2cf51c4c3
+ 
 app.listen(port, () => {
   console.log(`App running at port :${port}`);
 });
